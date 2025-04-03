f8c62f9506829ae967926da178cc77bfdd3fe738
fuszz <uliaszfilip@gmail.com>
2025-04-03 23:45:46 +0200

    Fix name conflic

--- lab4/Exercise3.java
+++ lab4/Exercise3.java
@@ -3,7 +3,7 @@ package lab4;
 import java.util.concurrent.Semaphore;
 
 
-class Buffer extends Thread{
+class Buffer1 extends Thread{
     static Semaphore existValue = new Semaphore(0);
     static Semaphore isFree = new Semaphore(1);
     float value;
@@ -24,10 +24,10 @@ class Buffer extends Thread{
 }
 
 class Consumer extends Thread {
-    Buffer b;
+    Buffer1 b;
 
 
-    Consumer(Buffer b){
+    Consumer(Buffer1 b){
         this.b = b;
     }
 
@@ -51,10 +51,10 @@ class Consumer extends Thread {
 
 
 class Producer extends Thread {
-    Buffer buffer;
+    Buffer1 buffer1;
 
-    Producer(Buffer buffer){
-        this.buffer = buffer;
+    Producer(Buffer1 buffer1){
+        this.buffer1 = buffer1;
     }
 
     float produce(){
@@ -72,7 +72,7 @@ class Producer extends Thread {
             try{sleep(500);} catch (InterruptedException e) {};
             float value = produce();
             System.out.println("Wyprodukowałem " + value);
-            buffer.setValue(value);
+            buffer1.setValue(value);
         }
     }
 
@@ -82,7 +82,7 @@ class Producer extends Thread {
 
 public class Exercise3 {
     public static void main(String[] args) {
-        Buffer b = new Buffer();
+        Buffer1 b = new Buffer1();
         Consumer c = new Consumer(b);
         Producer p = new Producer(b);
 
